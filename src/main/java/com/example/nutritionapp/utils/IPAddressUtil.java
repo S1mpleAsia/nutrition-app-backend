7308d0ab48bbd4e552e73bbd328e13e039c97677
S1mpleAsia <duongk65bkhn@gmail.com>
2023-12-12 19:51:33 +0700

    update vps ip v2

--- src/main/java/com/example/nutritionapp/utils/IPAddressUtil.java
+++ src/main/java/com/example/nutritionapp/utils/IPAddressUtil.java
@@ -1,12 +1,22 @@
 package com.example.nutritionapp.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.UnknownHostException;
 
 public class IPAddressUtil {
-    public static String getIPAddress() throws UnknownHostException {
-        InetAddress address = InetAddress.getLocalHost();
+    public static String getIPAddress() throws MalformedURLException {
+        String urlString = "http://checkip.amazonaws.com/";
+        URL url = new URL(urlString);
 
-        return address.getHostAddress();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
